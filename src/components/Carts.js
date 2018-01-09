cbc54064e59363380f8f31853558e37cb7e4f986
bokiledobri <bokiledobri@github.com>
2018-01-09 02:15:09 +0100

    imported bootstrap

--- src/components/Carts.js
+++ src/components/Carts.js
@@ -3,6 +3,9 @@ import React from 'react'
 const Carts = props=>{
     return(
         <div>
+        <p>{}props.ID</p>
+        <p>{props.ime}</p>
+        <p>{props.naslov}</p>
         </div>
     )
 }
